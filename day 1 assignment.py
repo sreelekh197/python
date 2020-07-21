5b6c2edb1bd8b869b9b5b0c19f5344a3dd076bdc
sreelekh197 <68578564+sreelekh197@users.noreply.github.com>
2020-07-21 10:33:11 +0530

    Update and rename README.md to day 1 assignment.py

---
+++ day 1 assignment.py
@@ -0,0 +1,9 @@
+What is python? what are the ways to install python in your pc?
+A: Python is an intrepreted ,high-level, general-purpose programming language.
+Two ways to install python:1) we can directly install through python.org
+                           2) we will install free anaconda distribution. anaconda distribution includes python as well as many other usefull libraries,                                   including IDEs like jupyer notebook, pycharm.
+What are code Environments? Types?
+Code environment is a place where we can right the python code, excute, o/p of the code.
+3 types: 1) Text editor
+2) Full IDEs
+3) Notebook environments. 
